7547e8d2cb00227e006824c1552de52420448827
dodsonm <mdodson1023@gmail.com>
2015-10-12 13:13:20 -0400

    add a basic weapon form using mongoose schema

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    Weapon = require('../models/Weapon'),
+    router = express.Router();
+
+// HOMEPAGE
+router.get('/', function(req, res) {
+    res.render('index', {
+        title:'Weapons Guide | Fire Emblem | Awakening',
+        credit: 'Matt.Dodson.Digital',
+        msg: 'Hello Word!'
+    });
+});
+
+// WEAPON
+router.get('/weapon', function(req, res) {
+  var schema = {};
+  Weapon.schema.eachPath(function (pathname, schemaType) {
+    schema[pathname] = schemaType.instance;
+  });
+
+    res.render('weapon', {
+        title:'Weapons Guide | Fire Emblem | Awakening',
+        credit: 'Matt.Dodson.Digital',
+        msg: 'This is the weapon form page.',
+        schema: schema
+    });
+});
+
+module.exports = router;
